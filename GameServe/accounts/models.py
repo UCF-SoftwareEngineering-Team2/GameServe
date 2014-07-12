fc220795a6e7f1c41bc700a8af9507e1e92f0afc
James Choi <jchoi2012@knights.ucf.edu>
2014-07-12 19:30:50 -0400

    Update models.py
    little fix

--- GameServe/accounts/models.py
+++ GameServe/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phonenumber_field.formfields import PhoneNumberField
+
 
 # Create your models here.
 class UserProfile(models.Model):
