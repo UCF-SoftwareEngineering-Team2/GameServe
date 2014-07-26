063d0be0761ae4f86200c39fe54582840dc310d5
James Choi <jchoi2012@knights.ucf.edu>
2014-07-26 04:57:25 -0400

    Make the navigation links work
    Can't believe it took so long to do this

--- GameServe/static/js/header.js
+++ GameServe/static/js/header.js
@@ -1,30 +1,58 @@
 $(document).ready(function() {
 
-	//login functionality
-	$('#login').click(function () {
-		console.log("Clicked");
-		console.log ($(this).next('.linkSlider').hasClass('active'));
-		// if $('.linkSlider').hasClass('active') {
-	 //  		$('#loginDropdown').toggleClass('hidden');
-		// }
-	  	$('#loginDropdown').toggleClass('hidden');
-
-	});
 
 	$('.icon-close').click(function () {
 	  	$('#popupWindow').toggleClass('hidden');
 	  	$('#content').toggleClass('blurry');
 	});
 
-	$('#loginButton').click(function () {
-		var email =	$('#emailInput').val();
-		var pass = $('#passwordInput').val();
-
-		alert("data: \email\t" + email + "\npass:\t" + pass);
+	// I don't think registration button belongs in the popup
+	$('#newUser').click(function() {
+		alert("redirect to creat_account page");
 	});
 
 
-	$('#newUser').click(function() {
-		alert("redirect to creat_account page");
+
+	// Login form submission
+	$('#loginButton').click(function () {
+		var email =	$('#emailInput').val();
+		var pass = $('#passwordInput').val();
+		alert("data: \email\t" + email + "\npass:\t" + pass);	
+	});
+	
+	
+	// Navigation bar clicks handlers
+	$('.headerLink').click(function(){
+		// Get current path
+		var pathname = window.location.pathname ; 
+
+		// login navi button was clicked
+		if ( this.id === 'login'){
+			$('#loginDropdown').toggleClass('hidden');
+		}
+		// Logout button clicked
+		else if (this.id === 'logout') {
+			window.location.href='/accounts/logout/';
+		}
+		// Home button clicked
+		else if (this.id === "home"){
+			if (pathname !== '/' && pathname !== '/events/') window.location.href = '/' ;
+		}
+		// Browse button
+		else if (this.id === 'browse'){
+			if (pathname !== '/events/browse/') window.location.href = "/events/browse/";
+		}
+		// Register button 
+		else if ( this.id === 'register'){
+			if (pathname !== '/accounts/register/') window.location.href = '/accounts/signup/';
+		}
+		// create events button (shown only if user is logged in)
+		else if ( this.id === 'create'){
+			if (pathname !== '/events/create/') window.location.href = '/events/create/';	
+		}
+		// Nada
+		else {
+			return ;
+		}
 	});
 });
